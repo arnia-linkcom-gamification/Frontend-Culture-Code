c12391665781f7857db010ad160e3690b81dce8f
Alexdosantos <alexdosantoss@yahoo.com>
2024-02-18 20:58:33 -0300

    Fix: Correção no layout cardProduct

--- src/components/cardProduct/CardProductStyled.tsx
+++ src/components/cardProduct/CardProductStyled.tsx
@@ -22,7 +22,7 @@ export const DivTitle = styled.div`
   gap: 5px;
 
   @media screen and (max-width: 728px) {
-    margin: 0 0 0 0;
+    margin: 1% 0 0 0;
   }
 `;
 export const DivImg = styled.div`
