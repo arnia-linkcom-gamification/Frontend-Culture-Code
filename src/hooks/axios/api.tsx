90f55ceaec96af273ac386b9c4295142cc372794
Alex dos Santos <125704045+Alexdosantos@users.noreply.github.com>
2024-04-03 20:46:24 -0300

    Update api.tsx
    Nova url api

--- src/hooks/axios/api.tsx
+++ src/hooks/axios/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 const accessToken = localStorage.getItem("access_token") || "";
 export const api = axios.create({
-  baseURL: "https://backend-culture-code-production.up.railway.app",
+  baseURL: "https://back-end-module-4.onrender.com",
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${accessToken}`,
