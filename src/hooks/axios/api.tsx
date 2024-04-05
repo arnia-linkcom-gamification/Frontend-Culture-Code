07760c2a27308269f8f873a719ae6f60449e0c33
Alex dos Santos <125704045+Alexdosantos@users.noreply.github.com>
2024-04-04 22:59:57 -0300

    Update api.tsx

--- src/hooks/axios/api.tsx
+++ src/hooks/axios/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 const accessToken = localStorage.getItem("access_token") || "";
 export const api = axios.create({
-  baseURL: "https://api-gamification.onrender.com/",
+  baseURL: "https://api-gamification.onrender.com",
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${accessToken}`,
